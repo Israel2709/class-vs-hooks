b87b9e9aaeca6d754c28510ba89c0c620df91e20
Israel Salinas Martínez <israel.salinas.m@gmail.com>
2020-10-10 05:15:58 -0500

    db connection, realtime post update

--- src/Components/ClassComponents/MainPage.jsx
+++ src/Components/ClassComponents/MainPage.jsx
@@ -6,71 +6,96 @@ class MainPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            posts:{},
-            title:"Mi primer post",
-            author:"Raziel Iqui Balam",
-            img:"https://picsum.photos/id/237/300",
-            content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore impedit, quo corporis voluptate cupiditate dignissimos!"
+            posts: {},
+            title: "Mi primer post",
+            author: "Raziel Iqui Balam",
+            img: "https://picsum.photos/id/237/300",
+            content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore impedit, quo corporis voluptate cupiditate dignissimos!"
         }
         this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
 
-    componentDidMount(){
-        fetch("https://class-vs-hooks.firebaseio.com/.json").then( response => {
-            response.json().then( json => {
-                this.setState( { posts : json })
+    getPosts = () => {
+        fetch("https://class-vs-hooks.firebaseio.com/posts/.json").then(response => {
+            response.json().then(json => {
+                this.setState({ posts: json })
             })
         })
     }
 
-    handleChange( event ){
+    componentDidMount() {
+        this.getPosts()
+    }
+
+    handleChange(event) {
         let property = event.target.name
         let value = event.target.value
-        this.setState( { [property] : value } )
+        this.setState({ [property]: value })
+    }
+
+    handleSubmit() {
+        let { title, author, content, img } = this.state
+        let postObject = { title, author, content, img }
+        fetch("https://class-vs-hooks.firebaseio.com/posts/.json", {
+            method: "POST",
+            body: JSON.stringify(postObject)
+        }).then(response => {
+            console.log(response)
+            this.getPosts()
+        })
     }
 
     render() {
-        let { title, content, img, author } = this.state
+        let { title, content, img, author, posts } = this.state
         return (
             <Container fluid>
                 <Row>
                     <Col md="4">
-                        <Form className="p-3 bg-dark text-white mt-3">
+                        <Form className="p-3 bg-dark text-white my-3">
                             <FormGroup>
                                 <Label for="title">Título</Label>
-                                <Input type="text" name="title" id="title" value={title} onChange={this.handleChange}/>
+                                <Input type="text" name="title" id="title" value={title} onChange={this.handleChange} />
                             </FormGroup>
                             <FormGroup>
                                 <Label for="author">Autor</Label>
-                                <Input type="text" name="author" id="author" value={author} onChange={this.handleChange}/>
+                                <Input type="text" name="author" id="author" value={author} onChange={this.handleChange} />
                             </FormGroup>
                             <FormGroup>
                                 <Label for="img">Imagen (URL) </Label>
-                                <Input type="text" name="img" id="img" value={img} onChange={this.handleChange}/>
+                                <Input type="text" name="img" id="img" value={img} onChange={this.handleChange} />
                             </FormGroup>
                             <FormGroup>
                                 <Label for="content">Contenido</Label>
-                                <Input type="textarea" name="text" id="content" value={content} onChange={this.handleChange}/>
+                                <Input type="textarea" name="text" id="content" value={content} onChange={this.handleChange} />
                             </FormGroup>
+                            <Button type="button" color="primary" className="ml-auto d-block" onClick={this.handleSubmit}>Crear post</Button>
                         </Form>
                     </Col>
                     <Col md="8">
-                        <Card>
-                            <Row>
-                                <Col xs="3">
-                                    <CardImg top src="https://picsum.photos/id/237/300" alt="Card image cap" className="full-size-pic"/>
-                                </Col>
-                                <Col xs="9">
-                                    <CardBody>
-                                        <CardTitle>Card Title</CardTitle>
-                                        <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-                                        <CardText>
-                                            <small className="text-muted">Last updated 3 mins ago</small>
-                                        </CardText>
-                                    </CardBody>
-                                </Col>
-                            </Row>
-                        </Card>
+                        {
+                            Object.keys(posts).map(post => {
+                                let { title, author, content, img } = posts[post]
+                                return (
+                                    <Card className="mb-3">
+                                        <Row>
+                                            <Col xs="3">
+                                                <CardImg top src={img} alt="Card image cap" className="full-size-pic" />
+                                            </Col>
+                                            <Col xs="9">
+                                                <CardBody >
+                                                    <CardTitle>{title}</CardTitle>
+                                                    <CardText>{content}</CardText>
+                                                    <CardText>
+                                                        <small className="text-muted">{author}</small>
+                                                    </CardText>
+                                                </CardBody>
+                                            </Col>
+                                        </Row>
+                                    </Card>
+                                )
+                            })
+                        }
                     </Col>
                 </Row>
             </Container>
