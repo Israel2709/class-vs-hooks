d7bfb405debe764d3c698531e5c87b8fff18fc83
Israel Salinas Martínez <israel.salinas.m@gmail.com>
2020-10-10 04:15:23 -0500

    hello koders

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import MainPage from "./Components/ClassComponents/MainPage"
 
 function App() {
   return (
     <div className="App">
-      
+      <MainPage/>
     </div>
   );
 }
---
+++ src/Components/ClassComponents/MainPage.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class MainPage extends Component {
+    constructor( props ){
+        super( props )
+    }
+    render(){
+        return(
+            <h1>Hola koders</h1>
+        )
+    }
+}
+
+export default MainPage
